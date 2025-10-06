f37f7c9511b33b11be3dd7ff68b8a76956d7607a
Jorge N <iajsenac@gmail.com>
2025-10-06 16:58:11 -0300

    proj

---
+++ UC4/projeto/proj.py
@@ -0,0 +1,72 @@
+
+
+def menu():
+    print("\t=======MENU PRINCIPAL=======")
+    print("1. Exercícios resolvidos de Estatística (PDF) ")
+    print("2. Quiz: Estatística em IA ")
+    print("3. Quiz: Mhachine learning ")
+    print("4. Pesquisa sobre uso de IA (PDF) ")
+    print("0. Sair")
+    
+while True:
+    menu()
+    escolha = input("Escolha uma opção: ")
+    
+    if escolha == "0":
+        print("Saindo...")
+        break
+    
+    elif escolha == "1":
+        print("")
+        os.startfile("UC4/projeto/,,,,,.pdf")
+        input("Pressione enter para voltar ao Menu Principal")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
